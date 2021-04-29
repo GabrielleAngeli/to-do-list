b4f515527b6b679946a2e8ced44675ebf8e1199b
gabrielle.poli <gabrielleangelipoli@gmail.com>
2021-04-28 22:55:19 -0300

    Creating the viewModel

---
+++ app/src/main/java/com/example/todolist/screens/AddItemFragment.kt
@@ -0,0 +1,19 @@
+package com.example.todolist.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.todolist.R
+
+class AddItemFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.add_item_fragment, container, false)
+    }
+}
